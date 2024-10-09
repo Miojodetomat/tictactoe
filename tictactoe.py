271270059cabcfd441f1d01abcd421266e95177d
Hugo Gomes Soares <cc22136@g.unicamp.br>
2024-10-09 13:52:25 -0300

    feat: implementing methods to control the state of the game

--- tictactoe.py
+++ tictactoe.py
@@ -3,20 +3,23 @@ import math
 X = 'X'
 O = 'O'
 EMPTY = None
-
+vezDe = 'O'
 
 def initial_state():
     '''
     returns starting state of the board.
     '''
-    raise NotImplementedError
+    return [[EMPTY for i in range(3)] for j in range(3)] #preenche com EMPTY as linhas e colunas
 
 
 def playear(board):
     '''
     returns player who has the next turn on a board.
     '''
-    raise NotImplementedError
+    if vezDe == X: #se a vez atual for de X então o próximo será O
+        return O
+    else  #se a vez atual for de O então o próximo será X
+        return X
 
 
 def actions(board):
@@ -30,29 +33,93 @@ def result(board, action):
     '''
     returns the board that results from making move (i, j) on the board.
     '''
-    raise NotImplementedError
+    i, j = actions
+    if vezDe == X:
+        vezDe = O #altera a vez devido ação realizada
+        board[i][j] = X #atualiza a mudança do tabuleiro
+        return board
+    else:
+        vezDe = X #altera a vez devido ação realizada
+        board[i][j] = O #atualiza a mudança do tabuleiro
+        return board
 
 
 def winner(board):
     '''
     returns the winner of the game, if there is one.
     '''
-    raise NotImplementedError
+    winner = utility(board)
+    if winner != 0:
+        if winner == 1:
+            return X
+        elif winner == -1:
+            return O
+    else
+        return None
 
 
 def terminal(board):
     '''
     returns True if game is over, False otherwise.
     '''
-    raise NotImplementedError
+    hasWinner = winner(board)
+    if hasWinner is not None:
+        return True
+
+    for i in range(3):
+        for j in range(3):
+            if board[i][j] == EMPTY:
+                return False
+    return True
+
 
 
 def utility(board):
     '''
     returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     '''
-    raise NotImplementedError
+    for i in range(3):
+        count = 1
+        aux = board[i][0]
+        for j in range(1,3):
+            if board[i][j] == aux:
+                count += 1
+            else
+                break
+        if count == 3:
+            if aux == X:
+                return 1
+            else
+                return -1
 
+    for j in range(3):
+        count = 1
+        aux = board[0][j]
+        for i in range(1,3):
+            if board[i][j] == aux:
+                count += 1
+            else
+                break
+        if count == 3:
+            if aux == X:
+                return 1
+            else
+                return -1
+
+    mid = board[1][1]
+    if board[0][0] == mid and board[2][2] == mid:
+        if mid == X:
+                return 1
+            else
+                return -1
+
+    if board[2][0] == mid and board[0][2] == mid:
+        if mid == X:
+                return 1
+            else
+                return -1
+
+    return 0
 
 def minimax(board):
     '''
