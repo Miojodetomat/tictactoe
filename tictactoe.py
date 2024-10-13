ea2e5827bc13709ae89758d198ee3243a1d8c0d0
Júlia Enriquetto de Brito <107726291+juliaenriquetto@users.noreply.github.com>
2024-10-13 18:47:47 -0300

    style: update the comments to the english version

--- tictactoe.py
+++ tictactoe.py
@@ -138,46 +138,47 @@ def utility(board):
 
     return 0
 
-def minimax(board): #algoritmo recursivo (árvore)
+def minimax(board): # recursive decision tree algorithm 
     '''
     returns the optimal action for the current player on the board.
     '''
-    #primeira chamada do método => onde a ação tomada é a relevante
+    # first call of the method => where the action taken is the relevant one
     cPlayer = player(board)
-    if cPlayer == X: #player quer maximizar o resultado
-        value = -2  #valor menor que todos os valores possíveis [-1, 0, 1]
+    if cPlayer == X: # player wants to maximize the result
+        value = -2  # value smaller than all possible values [-1, 0, 1]
         bestAction = None
         for a in actions(board):
-            newValue = insideMinimax(result(board, a)) #encontra os desdobramentos da ação tomada, buscando a decisão que maximiza o value
-            if newValue > value: #verifica se o novo valor é maior que o anterior
-                value = newValue #maximiza o valor
-                bestAction = a   #armazena a melhor ação correspondente à maximização
-        return bestAction # retorna a melhor ação encontrada
+            newValue = insideMinimax(result(board, a)) # finds the outcomes of the action taken, seeking the decision that maximizes the value
+            if newValue > value: # checks if the new value is greater than the previous one
+                value = newValue # maximizes the value
+                bestAction = a   # stores the best action corresponding to the maximization
+        return bestAction # returns the best action found
+    
+    if cPlayer == O: # player wants to minimize the result
+        value = 2 # value greater than all possible values [-1, 0, 1]
+        bestAction = None
+        for a in actions(board):
+            newValue = insideMinimax(result(board, a)) # finds the outcomes of the action taken, seeking the decision that minimizes the value
+            if newValue < value: # checks if the new value is greater than the previous one
+                value = newValue # minimizes the value
+                bestAction = a   # stores the best action corresponding to the minimization
+        return bestAction # returns the best action found
+    
+def insideMinimax(board): # internal recursion (I’m unsure how to make this method private in Python, or if it's even possible, but I believe that would be ideal. *How can I do this, Guilherme?)
 
-    if cPlayer == O: #player quer minimizar o resultado
-        value = 2 #valor maior que todos os valores possíveis [-1, 0, 1]\s
-        bestAction = None
-        for a in actions(board):
-            newValue = insideMinimax(result(board, a)) #encontra os desdobramentos da ação tomada, buscando a decisão que minimiza o value
-            if newValue < value: #verifica se o novo valor é maior que o anterior
-                value = newValue #minimiza o valor
-                bestAction = a   #armazena a melhor ação correspondente à minimização
-        return bestAction # retorna a melhor ação encontrada
-    
-def insideMinimax(board): #recursão interna (n sei como tornar esse metodo privado, nem sei se é possivel, mas é o ideal)
-    #condição de parada da recursão => quando alcançar o fim do jogo
+    # stop condition of the recursion => when reaching the end of the game
     if terminal(board):
-        return utility(board) #retorna quem foi o vencedor
+        return utility(board) # returns the winner
     
     cPlayer = player(board)
-    if cPlayer == X: #player quer maximizar o resultado
+    if cPlayer == X: # player wants to maximize the result
         value = -math.inf
         for a in actions(board):
-            value = max(value, insideMinimax(result(board, a))) #tenta encontrar a escolha que maximiza o value
-        return value # retorna a melhor ação encontrada
+            value = max(value, insideMinimax(result(board, a))) # tries to find the choice that maximizes the value
+        return value # returns the best action found
 
-    if cPlayer == O: #player quer minimizar o resultado
+    if cPlayer == O: # player wants to minimize the result
         value = math.inf
         for a in actions(board):
-            value = min(value, insideMinimax(result(board, a))) #tenta encontrar a escolha que minimize o value
-        return value # retorna a melhor ação encontrada
+            value = min(value, insideMinimax(result(board, a))) # tries to find the choice that minimizes the value
+        return value # returns the best action found
